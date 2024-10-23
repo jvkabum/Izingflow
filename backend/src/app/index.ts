03f7c4b7197a622e8ec404d5e0ccc5ac6060cce3
jvkabum <jvkabum@gmail.com>
2024-10-23 16:22:25 -0300

    anexo em mensagens rápidas
    atualização para anexo em mensagens rápidas backend

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -1,3 +1,5 @@
+// src/app/index.ts
+
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import "reflect-metadata";
@@ -7,9 +9,10 @@ import { createServer } from "http";
 import { env } from "process";
 import express from "express";
 import GracefulShutdown from "http-graceful-shutdown";
-import bootstrap from "./boot";
-import { initIO } from "../libs/socket";
+import bootstrap from "./boot"; // A função de bootstrap que você deve ter
+import { initIO } from "../libs/socket"; // Inicializa o socket
 import { StartAllWhatsAppsSessions } from "../services/WbotServices/StartAllWhatsAppsSessions";
+import configureExpress from './express'; // Importa a configuração do Express
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async function application() {
@@ -17,7 +20,9 @@ export default async function application() {
   const httpServer: any = createServer(app);
   const port = app.get("port") || env.PORT || 3100;
 
-  await bootstrap(app);
+  // Configura o Express
+  await configureExpress(app); // Adiciona configuração do Express
+  await bootstrap(app); // Executa o bootstrap
 
   async function start() {
     const host = app.get("host") || "0.0.0.0";
