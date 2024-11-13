2283a0c7ff3244a32419a42cad7be7b5507101ee
tiokabum <jvkabum@gmail.com>
2024-11-13 12:37:48 -0300

    Update ClosePendingTicketsJob.ts

--- backend/src/jobs/ClosePendingTicketsJob.ts
+++ backend/src/jobs/ClosePendingTicketsJob.ts
@@ -9,8 +9,8 @@ const closePendingTickets = async () => {
         
         // Verifica se o valor de daysToClose é válido
         if (daysToClose <= 0) {
-            console.error("Configuração inválida de dias para fechamento. O valor de 'daysToClose' deve ser maior que 0.");
-            return;
+        console.error("Configuração inválida de dias para fechamento. O valor de 'daysToClose' deve ser maior que 0.");
+        return;
         }
         
         // Calcular a data limite para fechamento dos Tickets
@@ -20,14 +20,17 @@ const closePendingTickets = async () => {
         console.log("Data de corte para fechamento:", cutoffDate.toISOString()); // Exibe a data no formato ISO
 
         // Parâmetros para a consulta
-        const queryParams = ['closed', 'pending', cutoffDate];
+        const queryParams = ['closed', 'pending', 'open', cutoffDate];
 
         // Log dos parâmetros antes da consulta
         console.log("Parâmetros da consulta:", queryParams);
 
-        // Atualiza Tickets que estão pendentes há mais do que o número de dias especificado
+        // Atualiza Tickets que estão Pendentes ou Abertos há mais do que o número de dias especificado
         const result = await db.query(
-            'UPDATE public."Tickets" SET status = $1 WHERE status = $2 AND "updatedAt" < $3',
+            `UPDATE public."Tickets"
+             SET status = $1
+             WHERE (status = $2 OR status = $3)
+             AND "updatedAt" < $4`,
             {
                 replacements: queryParams
             }
@@ -37,9 +40,9 @@ const closePendingTickets = async () => {
         const rowsAffected = result[1] as number;
 
         if (rowsAffected > 0) {
-            console.log(`Fechamento automático realizado para ${rowsAffected} ticket(s) pendente(s) há mais de ${daysToClose} dias.`);
+            console.log(`Fechamento automático realizado para ${rowsAffected} ticket(s) pendente(s)/aberto(s) há mais de ${daysToClose} dias.`);
         } else {
-            console.log("Nenhum ticket pendente foi encontrado para fechamento.");
+            console.log("Nenhum ticket pendente ou aberto foi encontrado para fechamento.");
         }
     } catch (error) {
         console.error("Erro ao fechar Tickets pendentes:", error.message || error);
